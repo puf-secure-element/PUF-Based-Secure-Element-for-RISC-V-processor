c4240e31eb9a949e13c4b6eee2eaa07970d1136a
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-07-12 15:20:15 +0700

    Add files via upload

---
+++ rtl/soc/soc.v
@@ -0,0 +1,107 @@
+module soc(
+    input   wire            clk,
+    input   wire            rst_n,
+
+    output  wire    [255:0] data_out,
+    output  wire            done
+);
+
+    parameter   SHA_ADDR_CTRL       = 8'h00;
+    parameter   SHA_ADDR_STATUS     = 8'h04;
+    // //Hash addresses
+    // parameter   SHA_ADDR_HASH0      = 8'h20;
+    // parameter   SHA_ADDR_HASH7      = 8'h27;
+
+    parameter   AES_ADDR_CTRL       = 8'h08;    //Bit 0 to control decryption, bit 1 to control encryption
+
+    parameter   ECC_ADDR_CTRL       = 8'hC;     //Bit 0 to control mode, 0: Enrollment, 1: Reconstruction
+
+    parameter   PUF_ADDR_CTRL       = 8'h10;    //Control PUF, write 1 to bit 0 to start transfer challenges
+
+    wire            key_is_ready, ecc_valid;
+    wire    [7:0]   address;                    //Address written to control modules
+
+    //AES internal signals
+    wire            de, en;
+
+    //SHA internal signals
+    wire            sha_error;
+    wire    [255:0] key;
+
+    //RV32 internal signals
+    wire            error_flag;
+    wire    [31:0]  rv_address;
+    wire    [31:0]  rv_rdata;
+    wire    [31:0]  rv_wdata;
+
+    //ECC internal signals
+    wire            mode;
+    wire    [511:0] ecc_response;
+
+    //PUF internal signals
+    wire            puf_valid;
+
+
+    assign  address =   rv_address[7:0];
+    //AES
+    assign  de      =   ((address == AES_ADDR_CTRL) && (rv_wdata[0] == 1)) ? 1'b1 : 1'b0;  //wdata[0] = 0
+    assign  en      =   ((address == AES_ADDR_CTRL) && (rv_wdata[1] == 1)) ? 1'b1 : 1'b0;  //wdata[1] = 1
+    //ECC
+    assign  mode    =   ((address == ECC_ADDR_CTRL)) ? rv_wdata[0] : 1'bz;  //wdata[0] = 1
+
+    aes dut(
+        .clk(clk),
+        .rst_n(rst_n),
+
+        .decrypt(de),
+        .encrypt(en),
+
+        .plaintext(),       //4 Instructions delay 4 clk to create 128 bits plaintext
+
+        .key_is_ready(key_is_ready),
+        .key_in(key),
+        //Encrypt/Decrypt value
+        .data_out(data_out),
+        .done(done)
+    );
+
+    sha256_top sha256(
+        .clk(clk),
+        .rst_n(rst_n),
+
+        .sel(1'b1),
+        .we(1'b1),
+
+        .addr(address),
+        .wdata(rv_wdata),
+
+        .ecc_response(ecc_response),
+        .ecc_valid(ecc_valid),
+
+        .rdata(rv_rdata),
+        .error(sha_error),
+        .hash_out(key),
+        .hash_valid(key_is_ready)
+    );
+
+    RV32I rv32(
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+
+    ecc_top ecc(
+        .clk_i(clk),
+        .rst_n_i(rst_n),
+        .mode_i(mode),
+        .start_i(puf_valid),
+        .raw_resp_i(),
+        .helper_in_i(),
+        .helper_val_i(),
+
+        .helper_out_o(),
+        .corr_resp_o(ecc_response),
+        .corr_resp_val_o(ecc_valid)
+    );
+
+
+endmodule

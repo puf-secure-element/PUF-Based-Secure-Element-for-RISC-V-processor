aa820c0dcac69d9d35d605eb2fa98091f9ca725d
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-07-13 10:01:55 +0700

    Add files via upload

--- sim/tb.f
+++ sim/tb.f
@@ -1,1 +1,11 @@
-+incdir+../tb/unit_tests
++incdir+${ECC_VIP_VERIF_PATH}/tb/uvm
++incdir+${ECC_VIP_VERIF_PATH}/vip/ecc_vip
++incdir+${ECC_VIP_VERIF_PATH}/sequences
++incdir+${ECC_VIP_VERIF_PATH}/testcases
+
+-f ${ECC_VIP_ROOT}/ecc_vip.f
+
+${ECC_VIP_VERIF_PATH}/tb/uvm/ecc_env_pkg.sv
+${ECC_VIP_VERIF_PATH}/sequences/seq_pkg.sv
+${ECC_VIP_VERIF_PATH}/testcases/test_pkg.sv
+${ECC_VIP_VERIF_PATH}/tb/uvm/testbench.sv

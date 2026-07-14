702a4cacc9e585db6dc700d0e3846364539801fe
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-07-14 14:35:24 +0700

    Add files via upload

--- sim/rtl.f
+++ sim/rtl.f
@@ -2,8 +2,12 @@
 +incdir+../rtl/aes_dual_rail/aes_core
 +incdir+../rtl/ecc
 +incdir+../rtl/riscv_core
++incdir+../rtl/puf
++incdir+../rtl/soc
 
 ../rtl/sha/*.v
 ../rtl/ecc/*.v
 ../rtl/riscv_core/*.v
+../rtl/puf/*.sv
 ../rtl/aes_dual_rail/aes_core/*.v
+../rtl/soc/*.v

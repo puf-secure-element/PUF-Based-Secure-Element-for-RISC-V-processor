63798e7781e8c59f65394a61902de7cb4db7b4c5
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-08-23 16:34:38 +0700

    Update tb_soc.sv

--- tb/unit_tests/tb_soc.sv
+++ tb/unit_tests/tb_soc.sv
@@ -10,7 +10,7 @@ module tb_soc;
         .clk(clk),
         .rst_n(rst_n),
         .data_out(data_out),
-        .done(done)
+        .irq(done)
     );
 
     //------------------------------------------------------
@@ -45,17 +45,17 @@ module tb_soc;
         last_aes = 'x;
     end
 
-    // always @(dut.puf_valid)
-    //     $display("[%0t] puf_valid = %b", $time, dut.puf_valid);
+    always @(dut.puf_valid)
+        $display("[%0t] puf_valid = %b", $time, dut.puf_valid);
 
-    // always @(dut.ecc_valid)
-    //     $display("[%0t] ecc_valid = %b", $time, dut.ecc_valid);
+    always @(dut.ecc_valid)
+        $display("[%0t] ecc_valid = %b", $time, dut.ecc_valid);
 
-    // always @(dut.key_is_ready)
-    //     $display("[%0t] key_ready = %b", $time, dut.key_is_ready);
+    always @(dut.key_is_ready)
+        $display("[%0t] key_ready = %b", $time, dut.key_is_ready);
 
-    // always @(dut.done)
-    //     $display("[%0t] aes_done = %b", $time, dut.done);
+    always @(dut.irq)
+        $display("[%0t] aes_done = %b", $time, dut.irq);
 
     always @(dut.puf_response or
              dut.ecc_response or
@@ -105,7 +105,7 @@ module tb_soc;
                 dut.dut.round,
                 dut.dut.key_index,
                 dut.dut.active,
-                dut.done,
+                dut.irq,
                 dut.data_out);
      end
 
@@ -117,10 +117,69 @@ module tb_soc;
     reg [31:0] tb_addr;
     reg [31:0] tb_data;
 
-    initial begin
-        force dut.rv_address = tb_addr;
-        force dut.rv_wdata   = tb_data;
-    end
+// reg [7:0]   last_sha_addr;
+// reg [31:0]  last_sha_wdata;
+// reg [511:0] last_sha_ecc;
+// reg [255:0] last_sha_key;
+// reg         last_sha_valid;
+
+// initial begin
+//     last_sha_addr  = 'x;
+//     last_sha_wdata = 'x;
+//     last_sha_ecc   = 'x;
+//     last_sha_key   = 'x;
+//     last_sha_valid = 'x;
+// end
+
+// always @(dut.address or
+//          dut.cpu_wdata or
+//          dut.ecc_response or
+//          dut.key or
+//          dut.key_is_ready)
+// begin
+
+//     if (dut.address !== last_sha_addr ||
+//         dut.cpu_wdata !== last_sha_wdata ||
+//         dut.ecc_response !== last_sha_ecc ||
+//         dut.key !== last_sha_key ||
+//         dut.key_is_ready !== last_sha_valid) begin
+
+//         $display("\n==============================================================");
+//         $display("[SHA CHANGE @ %0t]", $time);
+
+//         $display("  address       = %h",     dut.address);
+//         $display("  wdata         = %08h",   dut.cpu_wdata);
+
+//         $display("  ecc_response  = %0512h", dut.ecc_response);
+//         $display("  ecc_valid     = %b",     dut.ecc_valid);
+
+//         $display("  rdata         = %08h",   dut.cpu_rdata);
+//         $display("  error         = %b",     dut.sha_error);
+
+//         $display("  hash_out/key  = %0256h", dut.key);
+//         $display("  hash_valid    = %b",     dut.key_is_ready);
+
+//         $display("==============================================================");
+
+//         last_sha_addr  = dut.address;
+//         last_sha_wdata = dut.cpu_wdata;
+//         last_sha_ecc   = dut.ecc_response;
+//         last_sha_key   = dut.key;
+//         last_sha_valid = dut.key_is_ready;
+//     end
+// end
+
+// always @(posedge clk) begin
+//     $display(
+//         "[%0t] req=%b we=%b addr=%h wdata=%h ready=%b",
+//         $time,
+//         dut.cpu_req,
+//         dut.cpu_we,
+//         dut.cpu_addr,
+//         dut.cpu_wdata,
+//         dut.cpu_ready
+//     );
+// end
 
     task write_reg(input [31:0] addr, input [31:0] data);
     begin
@@ -144,17 +203,17 @@ module tb_soc;
 
         repeat(2) @(posedge clk);
 
-        // AES control = Encrypt
-        write_reg(32'h08, 32'h2);
+        // // AES control = Encrypt
+        // write_reg(32'h08, 32'h2);
 
-        // ECC mode = Enrollment
-        write_reg(32'h0C, 32'h0);
+        // // ECC mode = Enrollment
+        // write_reg(32'h0C, 32'h0);
 
-        // SHA start
-        write_reg(32'h00, 32'h1);
+        // // SHA start
+        // write_reg(32'h00, 32'h1);
 
-        // PUF start
-        write_reg(32'h10, 32'h1);
+        // // PUF start
+        // write_reg(32'h10, 32'h1);
 
         // Wait for system
         repeat(1000) @(posedge clk);

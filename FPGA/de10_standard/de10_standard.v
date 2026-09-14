3619e9ea24c795731d13843d9aa558ed42b3ab28
DND318 <dnd3108@gmail.com>
2026-09-14 11:47:02 +0700

    change FPGA config

--- FPGA/de10_standard/de10_standard.v
+++ FPGA/de10_standard/de10_standard.v
@@ -1,17 +1,23 @@
 module de10_standard (
     input  wire        CLOCK_50,
     input  wire [3:0]  KEY,
-    output wire [9:0]  LEDR
+    output wire [9:0]  LEDR,
+    input  wire        UART_RXD,
+    output wire        UART_TXD
 );
 
     wire [127:0] data_out;
     wire         irq;
+    wire         aes_done;
 
     soc u_soc (
-        .clk      (CLOCK_50),
-        .rst_n    (KEY[0]),
-        .data_out (data_out),
-        .irq      (irq)
+        .clk       (CLOCK_50),
+        .rst_n     (KEY[0]),
+        .data_out  (data_out),
+        .aes_done  (aes_done),
+        .uart_rxd  (UART_RXD),
+        .uart_txd  (UART_TXD),
+        .uart_irq  (irq)
     );
 
     assign LEDR[8:0] = data_out[8:0];
